972585e2abe9169a64af48eaa0a7351f0d5f2924
Portox04 <dibtecportocarrero@gmail.com>
2025-01-15 22:01:57 -0600

    Primera sincronizacion

---
+++ 2025C1/src/pkg2025c1/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2025c1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author porto
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int miPrimeraVariable;
+        String miSegundaVariable;
+        
+        miPrimeraVariable=99;
+        miSegundaVariable="Clase 1 Grupo";
+        
+        String variable="Hahaha";
+        String Variable="Hahaha";
+        String VARIABLE="Hahaha";
+        
+        String nombre;
+        int edad;
+        float salario;
+        double temperatura;
+        char inicial;
+          
+        
+        nombre = JOptionPane.showInputDialog("Por favor dame tu nombre");
+        
+    }
+    
+}
